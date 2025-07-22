6479878fab8e0934fea5480af808379a2c1ad6f3
Maria Mozgunova <maria@bumblebeeitsolutions.com>
2025-07-22 10:35:17 +0100

    Add shared Livewire traits

---
+++ src/Livewire/Traits/TrimAndNullEmptyStrings.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BBIT\Livewire\Traits;
+
+/**
+ * Replace empty strings with nulls.
+ * Livewire, unlike Laravel, does not replace empty strings with null values by default.
+ */
+trait TrimAndNullEmptyStrings
+{
+    public function updatedTrimAndNullEmptyStrings($name, $value)
+    {
+        if (is_string($value)) {
+            $value = trim($value);
+            $value = $value === '' ? null : $value;
+
+            data_set($this, $name, $value);
+        }
+    }
+}
---
+++ src/Livewire/Traits/WithSorting.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BBIT\Livewire\Traits;
+
+trait WithSorting {
+    public ?string $sortBy;
+    public $sortDirection = 'desc';
+
+    public function sort($column) {
+        if ($this->sortBy === $column) {
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        } else {
+            $this->sortBy = $column;
+            $this->sortDirection = 'asc';
+        }
+        $this->updated('sortBy', $this->sortBy);
+        $this->updated('sortDirection', $this->sortDirection);
+    }
+}
